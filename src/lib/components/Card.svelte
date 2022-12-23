206f52a83577776bd39bdd98d522c9c1b8e4d490
Michael <28806828+PumPum7@users.noreply.github.com>
2022-12-23 23:48:26 +0100

    split up the header component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let title: string;
+</script>
+
+<div>
+  <h2>{title}</h2>
+  <div class="card">
+    <slot />
+  </div>
+</div>
+
+
+<style lang="scss">
+  @use "../../variables" as var;
+
+  .card {
+    backdrop-filter: blur(1);
+    box-shadow: 0.5px 1px 1px hsl(223deg 5% 80% / 0.333);
+    border-radius: 16px;
+    border: 0.5px solid rgba(var.$nord9, 5%);
+    padding: 16px 22px;
+    background-color: var(--foreground-color)
+  }
+</style>
+
+
---
+++ src/lib/components/Header/Header.svelte
@@ -0,0 +1,44 @@
+<script>
+	import ThemeSwitch from "./ThemeSwitch.svelte";
+
+	let position;
+</script>
+
+<header style={position <= 90 ? "height: 100px;" : position <= 80 ? "height: 75px;" : "height: 70px;"}>
+	<div>
+		<p>Pum</p>
+
+		<ThemeSwitch />
+	</div>
+</header>
+
+<style lang="scss">
+	@use '../../../variables' as var;
+
+	header {
+		border-bottom: 1px solid;
+
+		position: sticky;
+		top: 0;
+
+		z-index: 1;
+		backdrop-filter: blur(8px);
+
+		transition: background-color 0.5s ease 0s, border-color 0.5s ease 0s, height 0.5s;
+	}
+
+	div {
+		@media (max-width: var.$breakpointSmall) {
+			width: 100%
+		}
+
+		height: 100%;
+		width: 50%;
+		margin: auto;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+</style>
+
+<svelte:window bind:scrollY={position} />
