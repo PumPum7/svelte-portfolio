36b1745e1385ceac1c0874e1c4a1c87c0da093e3
Michael <28806828+PumPum7@users.noreply.github.com>
2022-12-23 23:48:43 +0100

    layout fixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
-	import '../app.scss';
-	import Header from '../components/Header.svelte';
-	import Footer from '../components/Footer.svelte';
+	import "../app.scss";
+	import Header from "../lib/components/Header/Header.svelte";
+	import Footer from "../lib/components/Footer.svelte";
 </script>
 
 <Header />
@@ -19,7 +19,7 @@
 		}
 
 		width: 50%;
-		margin: auto;
+		margin: 10vh auto auto;
 
 		height: 10000px;
 	}
