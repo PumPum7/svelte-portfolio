bfac0d6488e366f572a1fb86397f742ff24ac3ba
Pum <28806828+PumPum7@users.noreply.github.com>
2024-05-09 16:46:13 +0200

    add speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,9 @@
 	import Footer from "../lib/components/Footer.svelte";
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
+	injectSpeedInsights();
 	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
