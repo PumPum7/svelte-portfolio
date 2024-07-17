25aede2d8752751f45667bdd210efa6f37e0dda9
Pum <28806828+PumPum7@users.noreply.github.com>
2024-07-17 10:56:25 +0200

    fix pre render statement position

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
-export const prerender = true;
-
 <script>
+	export const prerender = true;
+
 	import "../app.scss";
 	import Header from "../lib/components/Header/Header.svelte";
 	import Footer from "../lib/components/Footer.svelte";
